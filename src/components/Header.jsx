99f7be14c4a9db591f1b6ca8a10e7c898c727ada
ricardo <jojotas1234@gmail.com>
2025-05-17 16:06:04 -0300

    feat: adição de skeletons quando o server não está ligado, ou dá erro no fetch.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
-import {  } from 'react'
+import { useContext, useState } from 'react'
 import Navbar from '/projetos/projeto-ficai/src/components/Navbar'
+import { CidadesContext } from '../context/CidadesProvider'
+import { CircularProgress, Collapse, List, ListItemButton, ListItemText, Paper } from '@mui/material';
+import { CircleOutlined, ExpandLess, ExpandMore } from '@mui/icons-material';
 
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
+    const { cidades } = useContext(CidadesContext);
+  
+    const handleClick = () => {
+      setOpen(!open);
+    };
 
   return (
     <>
@@ -14,7 +23,59 @@ const Header = () => {
 
             <div className='flex space-x-4 gap-40'>
               <div>
-                <Navbar />
+                {cidades.length === 0 ? (
+        <div className="relative z-50">
+          <List component="nav" aria-label="main mailbox folders">
+            <ListItemButton
+              onClick={handleClick}
+              sx={{
+                backgroundColor: '#c87548',
+                color: 'white',
+                '&:hover': { backgroundColor: '#ffba01' },
+                borderRadius: 2,
+                border: 1,
+                borderColor: 'white',
+              }}
+            >
+              <ListItemText primary="Cidades" />
+              {open ? <ExpandMore /> : <ExpandLess />}
+            </ListItemButton>
+          </List>
+
+          <Collapse
+            in={open}
+            timeout="auto"
+            unmountOnExit
+            sx={{ position: 'absolute', top: '100%', left: 0, zIndex: 50, mt: 1 }}
+          >
+            <Paper
+              sx={{
+                width: 200,
+                borderRadius: 2,
+                boxShadow: 3,
+                overflow: 'hidden',
+              }}
+            >
+              <List 
+              component="div" 
+              disablePadding
+              sx={{
+                display:'flex',
+                justifyContent:'center',
+                alignItems:'center',
+                minHeight:'100'
+              }}
+              >
+                <CircularProgress />
+              </List>
+            </Paper>
+          </Collapse>
+        </div>
+                )
+
+                :
+                
+                (<Navbar />)}
               </div>
                 <ul className='flex space-x-4 mt-3'>
                     <li className='rounded-md'><a href="/Login" className='border-gray-400'><button className='text-white bg-(--color-secondary) rounded-md p-2 cursor-pointer outline-1 hover:bg-[#c87548]'>Entrar</button></a></li>
--- src/components/MainLP.jsx
+++ src/components/MainLP.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Cards from './Card'
+import { CidadesContext } from '../context/CidadesProvider'
+import { CardContent, Skeleton, Card, Typography, Button, CardActions, Box } from '@mui/material';
 
 const MainLP = () => {
+  const { cidades } = useContext(CidadesContext);
+
   return (
-    <Cards />
+    <>
+      <h3 className='mt-10 text-[30px] text-center font-bold'>Destinos Populares</h3>
+
+      {cidades.length != 0 ? (<Cards />) 
+      
+      :
+      <div className='grid grid-cols-3'>
+              <div className='m-6 w-150'>
+                  <Card className="flex flex-col justify-between h-full">
+                    <CardContent>
+                      <Skeleton variant='rectangular' height={300} width={560} sx={{borderRadius:3}} />
+                    </CardContent>
+                    <CardContent className="flex-1">
+                        <Typography variant="h5" component="div">
+                          {Array.from({ length: 1 }).map((_, i) => (
+                            <Skeleton key={i} height={50} sx={{ mb: 2 }} />
+                          ))}
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                          {Array.from({ length: 3 }).map((_, i) => (
+                            <Skeleton key={i} height={20} sx={{ mb: 1 }} />
+                          ))}
+                        </Typography>
+                    </CardContent>
+                </Card>
+              </div>
+              <div className='m-6 w-150'>
+                <Card className="flex flex-col justify-between h-full">
+                    <CardContent>
+                      <Skeleton variant='rectangular' height={300} width={560} sx={{borderRadius:3}} />
+                    </CardContent>
+                    <CardContent className="flex-1">
+                        <Typography variant="h5" component="div">
+                          {Array.from({ length: 1 }).map((_, i) => (
+                            <Skeleton key={i} height={50} sx={{ mb: 2 }} />
+                          ))}
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                          {Array.from({ length: 3 }).map((_, i) => (
+                            <Skeleton key={i} height={20} sx={{ mb: 1 }} />
+                          ))}
+                        </Typography>
+                    </CardContent>
+                </Card>
+              </div>
+              <div className='m-6 w-150'>
+                <Card className="flex flex-col justify-between h-full">
+                    <CardContent>
+                      <Skeleton variant='rectangular' height={300} width={560} sx={{borderRadius:3}} />
+                    </CardContent>
+                    <CardContent className="flex-1">
+                        <Typography variant="h5" component="div">
+                          {Array.from({ length: 1 }).map((_, i) => (
+                            <Skeleton key={i} height={50} sx={{ mb: 2 }} />
+                          ))}
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                          {Array.from({ length: 3 }).map((_, i) => (
+                            <Skeleton key={i} height={20} sx={{ mb: 1 }} />
+                          ))}
+                        </Typography>
+                    </CardContent>
+                </Card>
+              </div>
+      </div>
+      }
+    </>
   ) 
 }
 
