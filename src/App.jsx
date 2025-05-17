0b1879af25094f268cc727146c7df72076366d54
Luis Eduardo <luiseduardocbft@gmail.com>
2025-05-17 15:37:43 -0300

    Atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Landingpage from './pages/landingpage'
+import Fortaleza from './pages/Fortaleza'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Landingpage />} />
+      <Route path='/fortaleza' element={<Fortaleza />} />
     </Routes>
-  ) 
+  )
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <div className='flex justify-between bg-(color:--color-primary) p-4'>
-        <div className='ml-4'>
-            <a href="/"><img src="/src/img/ficaiicon.png" alt=""  className='h-10 cursor-pointer'/></a>
-        </div>
-        <div>
-            <nav className='mt-2'>
-                <ul className='flex justify-center space-x-4 gap-1'>
-                    <li><a href="" className='text-white hover:text-black'>Fortaleza</a></li>
-                    <li><a href="" className='text-white hover:text-black'>Guaramiranga</a></li>
-                    <li><a href="" className='text-white hover:text-black'>Aracati</a></li>
-                    <li><a href="" className='text-white hover:text-black'>Jicoca</a></li>
-                    <li><a href="" className='text-white hover:text-black'>Aquiraz</a></li>
+    return (
+        <div className='flex justify-between bg-(color:--color-primary) p-4'>
+            <div className='ml-4'>
+                <a href="/"><img src="/src/img/ficaiicon.png" alt="" className='h-10 cursor-pointer' /></a>
+            </div>
+            <div>
+                <nav className='mt-2'>
+                    <ul className='flex justify-center space-x-4 gap-1'>
+                        <li>
+                            <Link to="/fortaleza" className='text-white hover:text-black'>Fortaleza</Link>
+                        </li>
+                        <li><a href="" className='text-white hover:text-black'>Guaramiranga</a></li>
+                        <li><a href="" className='text-white hover:text-black'>Aracati</a></li>
+                        <li><a href="" className='text-white hover:text-black'>Jicoca</a></li>
+                        <li><a href="" className='text-white hover:text-black'>Aquiraz</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <ul className='flex space-x-4'>
+                    <li className='rounded-md'><a href="/Login" className='border-gray-400'><button className='text-white bg-orange-300 rounded-md p-2 cursor-pointer outline-1 hover:bg-amber-400 '>Entrar</button></a></li>
+                    <li className='p-2 bg-black rounded-md'><a href="/Cadastrar" className='text-white'>Cadastrar</a></li>
                 </ul>
-            </nav>
+            </div>
         </div>
-        <div>
-            <ul className='flex space-x-4'>
-                <li className='rounded-md'><a href="/Login" className='border-gray-400'><button className='text-white bg-orange-300 rounded-md p-2 cursor-pointer outline-1 hover:bg-amber-400 '>Entrar</button></a></li>
-                <li className='p-2 bg-black rounded-md'><a href="/Cadastrar" className='text-white'>Cadastrar</a></li>
-            </ul>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
