eb927272625a246cf3ff087a22b5fbab19682aa2
ricardo <jojotas1234@gmail.com>
2025-05-18 00:46:46 -0300

    imagens

---
+++ src/pages/Fortaleza.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fortaleza = () => {
+  return (
+    <div>Fortaleza</div>
+  )
+}
+
+export default Fortaleza
---
+++ src/pages/Jericoacoara.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Jericoacoara = () => {
+  return (
+    <div>Jericoacoara</div>
+  )
+}
+
+export default Jericoacoara
